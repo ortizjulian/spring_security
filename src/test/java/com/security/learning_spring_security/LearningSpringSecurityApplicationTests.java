e48da2c4ad22ced1a795aeff779282058911f039
Julian Ortiz <julian.ortiz@pragma.com.co>
2025-08-11 17:41:48 -0500

    spring initializr

---
+++ src/test/java/com/security/learning_spring_security/LearningSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.learning_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
